6656cb4473d67e368176dfebc13f4dc2d9e47b46
Ethan McMahan <cowboyfanet@hotmail.com>
2022-11-01 18:38:01 -0700

    started working on npm tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, num) {
+let word = "";
+    // i = 0, word = "", ${word}${str} = "hey"   
+    // i = 1, word = "hey", ${word}${str} = "heyhey"
+    for (let i = 0; i < num; i++) {
+        console.log(i);
+        word = `${word}${str}`;
+    }
+ return word;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, value) {
+    let index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
