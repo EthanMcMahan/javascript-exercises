f4c6a68f7af39f4260ab4a681a4102df758cd5ba
Ethan McMahan <cowboyfanet@hotmail.com>
2022-11-02 16:29:19 -0700

    updated exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -5,6 +5,9 @@ let word = "";
     for (let i = 0; i < num; i++) {
         console.log(i);
         word = `${word}${str}`;
+        if (num < 0) {
+            throw "num must not be negative";
+        }
     }
  return word;
 }
